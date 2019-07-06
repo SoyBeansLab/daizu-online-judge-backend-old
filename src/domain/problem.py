c7f71f2ca3ee1cf8fa2fc4f18cf957df27bd10c3
CAFF7182 <ic171226@edu.okinawa-ct.ac.jp>
2019-07-06 14:49:25 +0900

    feat: 入出力例解説を任意にした

--- src/domain/problem.py
+++ src/domain/problem.py
@@ -1,8 +1,8 @@
 class Problem:
     def __init__(self, problemName: str, problemText: str, problemConstraint: str,\
                        timeLimit: int, memoryLimit: int,\
-                       input: str, inputExam: str, inputComment: str,\
-                       output: str, outputExam: str, outputComment: str):
+                       input: str, inputExam: str, inputComment=None: str,\
+                       output: str, outputExam: str, outputComment=None: str):
 
         self.problemName = problemName
         self.problemText = problemText
