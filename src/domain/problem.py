899c5bad09493bb45edc60ceed86d847d8bc6ebf
CAFF7182 <ic171226@edu.okinawa-ct.ac.jp>
2019-07-09 11:50:09 +0900

    style: 整形しました

--- src/domain/problem.py
+++ src/domain/problem.py
@@ -1,9 +1,20 @@
 class Problem:
-    def __init__(self, contest_id: str, problem_id: str,\
-                       problem_name: str, problem_text: str, problem_constraint: str,\
-                       time_limit: int, memory_limit: int,\
-                       input_format: str, input_example: str, output_example: str, \
-                       output_format: str, input_comment=None, output_comment=None):
+    def __init__(
+        self,
+        contest_id: str,
+        problem_id: str,
+        problem_name: str,
+        problem_text: str,
+        problem_constraint: str,
+        time_limit: int,
+        memory_limit: int,
+        input_format: str,
+        input_example: str,
+        output_example: str,
+        output_format: str,
+        input_comment=None,
+        output_comment=None,
+    ):
 
         self.contest_id = contest_id
         self.problem_id = problem_id
@@ -14,11 +25,10 @@ class Problem:
         self.time_limit = time_limit
         self.memory_limit = memory_limit
 
-        self.input_format = input_format 
+        self.input_format = input_format
         self.input_example = input_example
         self.input_comment = input_comment
 
         self.output_format = output_format
         self.output_example = output_example
         self.output_comment = output_comment
-
