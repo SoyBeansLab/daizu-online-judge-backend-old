29ec474cefc08bb4270a306352e09a52829d436a
nemusou <ryubi.102395@gmail.com>
2019-08-18 20:15:47 +0900

    feat: responderでproblemAPIが動くようにした

--- src/infrastructure/waf/responder/router.py
+++ src/infrastructure/waf/responder/router.py
@@ -1,12 +1,14 @@
 import responder
 
 from infrastructure.database.postgres.sqlhandler import SqlHandler
-from interface.controllers.user_controller import UserController
+from interface.controllers.problem_controller import ProblemController
 
 
 def set_route(api: responder.API) -> None:
-    sqlhandler = SqlHandler("doj_user")
-    user_controller = UserController(sqlhandler)
+    problem_controller = ProblemController(SqlHandler("problem"))
 
-    api.add_route("/user/create", user_controller.create)
-    api.add_route("/users", user_controller.users)
+    api.add_route("/contest/{contest_id}/problems", problem_controller.problems)
+    api.add_route(
+        "/contest/{contest_id}/problems/{problem_id}",
+        problem_controller.problem,
+    )
