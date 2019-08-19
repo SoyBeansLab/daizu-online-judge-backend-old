82a5ee571831e16c208fa9721af133c50addc5a2
nemusou <ryubi.102395@gmail.com>
2019-08-19 23:58:28 +0900

    feat: responderにsubmittionのルーティングを追加した

--- src/infrastructure/waf/responder/router.py
+++ src/infrastructure/waf/responder/router.py
@@ -3,12 +3,14 @@ import responder
 from infrastructure.database.postgres.sqlhandler import SqlHandler
 from interface.controllers.contest_controller import ContestController
 from interface.controllers.problem_controller import ProblemController
+from interface.controllers.submittion_controller import SubmittionController
 from interface.controllers.user_controller import UserController
 
 
 def set_route(api: responder.API) -> None:
     set_route_contest(api)
     set_route_problem(api)
+    set_route_submittion(api)
     set_route_user(api)
 
 
@@ -34,3 +36,21 @@ def set_route_user(api: responder.API) -> None:
 
     api.add_route("/user/create", user_controller.create)
     api.add_route("/users", user_controller.users)
+
+
+def set_route_submittion(api: responder.API) -> None:
+    submittion_controller = SubmittionController(SqlHandler("submittion"))
+
+    api.add_route(
+        "/contest/{contest_id}/problems/{problem_id}/submit",
+        submittion_controller.submit,
+    )
+    # contest_idで紐づいたそのコンテストの提出リストとかほしい
+    api.add_route(
+        "/contest/{contest_id}/submittions/{problem_id}",
+        submittion_controller.submittions,
+    )
+    api.add_route(
+        "/contest/{contest_id}/submittions/{problem_id}/{submit_id}",
+        submittion_controller.submittion,
+    )
