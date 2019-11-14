9815bc0335060c2b2365c0db8e5f3515f89b41bc
nemusou <ryubi.102395@gmail.com>
2019-11-14 16:48:40 +0900

    feat: responderにOpenAPIの設定を追加した

--- src/app.py
+++ src/app.py
@@ -4,7 +4,14 @@ from infrastructure.waf.responder.router import set_route
 
 
 def main():
-    api = responder.API()
+    description: str = "daizu online judge developping is contest site of" \
+            " the competitive programming"
+    api = responder.API(
+        title="daizu online judge",
+        version="0.1.0",
+        openapi="3.0.2",
+        description=description
+    )
     set_route(api)
     api.run(address="0.0.0.0", port=5042)
 
