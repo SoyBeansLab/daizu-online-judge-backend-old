7d62380760f04c8a7d9999f2b049f99545755134
ucpr <contact@ucpr.dev>
2020-04-22 23:32:52 +0900

    style: formatting with black

--- src/interface/controllers/problem_controller.py
+++ src/interface/controllers/problem_controller.py
@@ -11,7 +11,7 @@ class ProblemController:
         problems = []
         for problem in self.interactor.problems(contest_id):
             problems.append(problem.as_dict())
-        resp.media =  problems
+        resp.media = problems
         resp.status_code = 200
 
     async def problem(self, req, resp, *, contest_id, problem_id):
