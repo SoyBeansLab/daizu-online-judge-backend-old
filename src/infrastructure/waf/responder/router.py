04ce9465a9aed366585e20ffe0fbf05934a668a7
ucpr <contact@ucpr.dev>
2020-04-22 09:40:57 +0900

    feat: add_schemaをコメントアウトした

--- src/infrastructure/waf/responder/router.py
+++ src/infrastructure/waf/responder/router.py
@@ -19,7 +19,8 @@ def set_route(api: responder.API) -> None:
 
 
 def set_schema(api: responder.API) -> None:
-    api.add_schema("User", UserSchema)
+#    api.add_schema("User", UserSchema)
+    pass
 
 
 def set_route_contest(api: responder.API) -> None:
