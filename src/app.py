ca51ed9c028639086a89958a87ad735e65b4c9b0
ucpr <contact@ucpr.dev>
2020-09-11 02:41:51 +0000

    feat: add log configuration

--- src/app.py
+++ src/app.py
@@ -1,7 +1,29 @@
+from logging import getLogger, StreamHandler, DEBUG, Formatter
+from os import environ
+
 from fastapi import FastAPI
 import uvicorn
 
 from infrastructure.waf.fastapi.router import set_route
+
+
+## Configuration Logger
+logger = getLogger("daizu")
+formatter = Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger.setLevel(DEBUG)
+logger.propagate = False
+
+if environ.get("DAIZU_LOG_STREAM", "") == "1":
+    handler = StreamHandler()
+    handler.setLevel(DEBUG)
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+if environ.get("DAIZU_LOG_FILE", "") == "1":
+    fh = FileHandler(filename='daizu_online_judge.log')
+    fh.setLevel(DEBUG)
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
 
 
 def main():
