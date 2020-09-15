e906789a26df5b0a8ade2bfa42e41ab89963d2a6
ucpr <contact@ucpr.dev>
2020-09-15 04:26:00 +0000

    feat: modify responses

--- src/interface/controllers/language_controller.py
+++ src/interface/controllers/language_controller.py
@@ -1,7 +1,14 @@
+from logging import getLogger
+
 from domain.Language.language import Language
 from domain.Language.database.language_repository import LanguageRepository
 from domain.Language.usecase.language_interactor import LanguageInteractor
 from infrastructure.database.postgres.sqlhandler import SqlHandler
+
+from exceptions.database import DuplicateKeyError
+from exceptions.waf import DuplicateKeyHTTPException
+
+logger = getLogger("daizu").getChild("LanguageController")
 
 
 class LanguageController:
@@ -10,27 +17,42 @@ class LanguageController:
 
     async def languages(self):
         languages = []
+        resp = dict()
         for lang in self.interactor.languages():
             languages.append(lang)
-        resp = {"languages": languages}
+        resp["data"] = languages
+        resp["status"] = "Success"
 
         return resp
 
     async def create_language(self, language: Language):
-        print(language)
-        self.interactor.store(language)
-        resp = {"status": "ok"}
+        resp = dict()
+        language_name = language.language
+        try:
+            self.interactor.store(language)
+            resp["status"] = "Success"
+            resp["message"] = "Create language"
+        except DuplicateKeyError as e:
+            message = f"Duplicate key. (Key: {language_name})"
+            logger.debug(message, e)
+            raise DuplicateKeyHTTPException(detail=message)
 
         return resp
 
     async def update(self, language: Language):
+        resp = {
+            "status": "Success",
+            "message": "Update language",
+        }
         self.interactor.update(language)
-        resp = {"status": "ok"}
 
         return resp
 
     async def delete(self, language_name: str):
+        resp = {
+            "status": "Success",
+            "message": f"Delete language.(language={language_name})",
+        }
         self.interactor.delete(language_name)
-        resp = {"status": "ok"}
 
         return resp
