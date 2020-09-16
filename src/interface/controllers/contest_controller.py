e2a633972a4e872b026ef15ec45af74437942c0b
ucpr <contact@ucpr.dev>
2020-09-17 07:23:02 +0900

    feat: modify response of contest

--- src/interface/controllers/contest_controller.py
+++ src/interface/controllers/contest_controller.py
@@ -1,8 +1,15 @@
+from logging import getLogger
+
 from domain.Contest.contest import Contest
 from domain.Contest.database.contest_repository import ContestRepository
 from domain.Contest.usecase.contest_interactor import ContestInteractor
 
 from interface.database.sqlhandler import SqlHandler
+
+from exceptions.database import DuplicateKeyError
+from exceptions.waf import DuplicateKeyHTTPException
+
+logger = getLogger("daizu").getChild("LanguageController")
 
 
 class ContestController:
@@ -14,6 +21,7 @@ class ContestController:
         recent_contests = []
         upcoming_contests = []
         current_contests = []
+        resp = dict()
 
         for contest in self.interactor.recent_contests():
             recent_contests.append(contest.as_json())
@@ -22,38 +30,57 @@ class ContestController:
         for contest in self.interactor.current_contests():
             current_contests.append(contest.as_json())
 
-        resp = {
+        resp["data"] = {
             "upcoming": upcoming_contests,
             "current": current_contests,
             "recent": recent_contests,
         }
+        resp["status"] = "Success"
+
         return resp
 
     async def contest(self, contest_id: str):
+        resp = dict()
         contest = self.interactor.contest(contest_id)
         if contest is None:
             raise self.HTTPException(
                 status_code=404, detail="contest not found"
             )
+        resp["data"] = contest.as_json()
+        resp["status"] = "Success"
 
-        res_data = contest.as_json()
-        resp = {"contest": res_data}
         return resp
 
     async def store(self, contest: Contest):
-        self.interactor.store(contest)
-        resp = {}
+        resp = dict()
+        contest_id = contest.contest_id
+
+        # The contest_id is determined by the admin so make sure there are no duplicates.
+        try:
+            self.interactor.store(contest)
+            resp["status"] = "Success"
+            resp["message"] = "Create contest"
+        except  DuplicateKeyError as e:
+            message = f"Duplicate key (Key: {contest_id})"
+            logger.debug(message, e)
+            raise DuplicateKeyHTTPException(detail=message)
 
         return resp
 
     async def update(self, contest_id: str, contest: Contest):
+        resp = {
+            "status": "Success",
+            "message": "Update contest",
+        }
         self.interactor.update(contest_id, contest)
-        resp = {}
 
         return resp
 
     async def delete(self, contest_id: str):
+        resp = {
+            "status": "Success",
+            "message": f"Delete contest (contest_id={contest_id})",
+        }
         self.interactor.delete(contest_id)
-        resp = {}
 
         return resp
