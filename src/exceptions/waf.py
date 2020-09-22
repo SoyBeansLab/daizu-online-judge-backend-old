e1ea3bf49e9278c8d0ca67cf274f7ac48fab1300
ucpr <contact@ucpr.dev>
2020-09-22 11:53:04 +0900

    feat: add api exceptions

--- src/exceptions/waf.py
+++ src/exceptions/waf.py
@@ -1,8 +1,41 @@
+from typing import List, Type, Dict
 from fastapi import HTTPException
+from fastapi.responses import JSONResponse
 
 
-class DuplicateKeyHTTPException(HTTPException):
-    def __init__(self, detail=""):
-        super(DuplicateKeyHTTPException, self).__init__(
-            status_code=400, detail=detail
-        )
+class APIError(Exception):
+    status_code: int = 404
+    detail: str = "Error"
+
+
+class DuplicateKeyHTTPException(APIError):
+    status_code = 400
+    detail = "Duplicate key"
+
+
+async def api_error_handler(request, err: APIError):
+    return JSONResponse(
+        status_code=err.status_code,
+        content={
+            "detail": err.detail,
+        },
+    )
+
+
+# https://tech.jxpress.net/entry/fastapi_error_definitions
+def error_response(error_types: List[Type[APIError]]) -> Dict:
+    d = {}
+    for et in error_types:
+        if not d.get(et.status_code):
+            d[et.status_code] = {
+                'description': f'{et.detail}',
+                'content': {
+                    'application/json': {
+                        'example': {
+                            'detail': et.detail
+                        }
+                    }
+                }}
+        else:
+            d[et.status_code]['description'] += f'<br>{et.detail}'
+    return d
