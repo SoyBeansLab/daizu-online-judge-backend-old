991e622fb77b2070f46807e26f4a2f9956400c5d
ucpr <contact@ucpr.dev>
2020-10-02 22:45:59 +0900

    style: run black

--- src/infrastructure/auth/auth0.py
+++ src/infrastructure/auth/auth0.py
@@ -57,11 +57,11 @@ def requires_auth(token, AUTH0_DOMAIN: str = ""):
     for key in jwks.get("keys"):
         if key["kid"] == unverified_header["kid"]:
             rsa_key = {
-                    "kty": key["kty"],
-                    "kid": key["kid"],
-                    "use": key["use"],
-                    "n": key["n"],
-                    "e": key["e"]
+                "kty": key["kty"],
+                "kid": key["kid"],
+                "use": key["use"],
+                "n": key["n"],
+                "e": key["e"],
             }
     if rsa_key:
         try:
@@ -69,20 +69,22 @@ def requires_auth(token, AUTH0_DOMAIN: str = ""):
                 token,
                 rsa_key,
                 algorithms=AUTH0_ALGORITHMS,
-    #            audience=AUTH0_AUDIENCE,
+                #            audience=AUTH0_AUDIENCE,
                 audience="6hQz2EfoLoWsLGjvf7AMqtohxY8CstO0",
                 issuer=f"https://{AUTH0_DOMAIN}/",
             )
         except jwt.ExpiredSignatureError:
             raise AuthError(detail="token is expired")
         except jwt.JWTClaimsError:
-            raise AuthError(detail="incorrect claims, please check the audience and issuer")
+            raise AuthError(
+                detail="incorrect claims, please check the audience and issuer"
+            )
         except Exception:
             raise AuthError(detail="Unable to parse authentication token.")
 
     #    _request_ctx_stack.top.current_user = payload
     #    return f(*args, **kwargs)
-    #raise AuthError(detail="Unable to find appropriate key")
+    # raise AuthError(detail="Unable to find appropriate key")
 
 
 def requires_scope(required_scope: str, token: str):
