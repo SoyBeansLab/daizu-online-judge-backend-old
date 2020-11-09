85886b59f071fa47b014515fd77c25eb3f1f1d8d
ucpr <contact@ucpr.dev>
2020-11-10 01:19:52 +0900

    style: run black

--- src/domain/Contest/test_helper.py
+++ src/domain/Contest/test_helper.py
@@ -25,55 +25,64 @@ def create_current_test_datetime() -> datetime:
 def create_recent_contests(n: int) -> List[Contest]:
     result = list()
     for i in range(n):
-        result.append(Contest(
-            contest_id=f"test_recent_{i}",
-            contest_name="TestRecentContest{i}",
-            contest_start_date=create_recent_test_datetime(),
-            contest_finish_date=create_recent_test_datetime() + timedelta(days=1),
-            contest_time=120,
-            writer="ucpr",
-            description="Welcome",
-            top_content="Welcome",
-            problem_number=4,
-            created_at=create_test_datetime(),
-            updated_at=create_test_datetime(),
-        ).as_json())
+        result.append(
+            Contest(
+                contest_id=f"test_recent_{i}",
+                contest_name="TestRecentContest{i}",
+                contest_start_date=create_recent_test_datetime(),
+                contest_finish_date=create_recent_test_datetime()
+                + timedelta(days=1),
+                contest_time=120,
+                writer="ucpr",
+                description="Welcome",
+                top_content="Welcome",
+                problem_number=4,
+                created_at=create_test_datetime(),
+                updated_at=create_test_datetime(),
+            ).as_json()
+        )
     return result
 
 
 def create_upcoming_contests(n: int) -> List[Contest]:
     result = list()
     for i in range(n):
-        result.append(Contest(
-            contest_id=f"test_upcoming_{i}",
-            contest_name="TestUpcomingContest{i}",
-            contest_start_date=create_upcoming_test_datetime(),
-            contest_finish_date=create_upcoming_test_datetime() + timedelta(days=1),
-            contest_time=120,
-            writer="ucpr",
-            description="Welcome",
-            top_content="Welcome",
-            problem_number=4,
-            created_at=create_test_datetime(),
-            updated_at=create_test_datetime(),
-        ).as_json())
+        result.append(
+            Contest(
+                contest_id=f"test_upcoming_{i}",
+                contest_name="TestUpcomingContest{i}",
+                contest_start_date=create_upcoming_test_datetime(),
+                contest_finish_date=create_upcoming_test_datetime()
+                + timedelta(days=1),
+                contest_time=120,
+                writer="ucpr",
+                description="Welcome",
+                top_content="Welcome",
+                problem_number=4,
+                created_at=create_test_datetime(),
+                updated_at=create_test_datetime(),
+            ).as_json()
+        )
     return result
 
 
 def create_current_contests(n: int) -> List[Contest]:
     result = list()
     for i in range(n):
-        result.append(Contest(
-            contest_id=f"test_current_{i}",
-            contest_name="TestCurrentContest{i}",
-            contest_start_date=create_current_test_datetime(),
-            contest_finish_date=create_current_test_datetime() + timedelta(days=1),
-            contest_time=120,
-            writer="ucpr",
-            description="Welcome",
-            top_content="Welcome",
-            problem_number=4,
-            created_at=create_test_datetime(),
-            updated_at=create_test_datetime(),
-        ).as_json())
+        result.append(
+            Contest(
+                contest_id=f"test_current_{i}",
+                contest_name="TestCurrentContest{i}",
+                contest_start_date=create_current_test_datetime(),
+                contest_finish_date=create_current_test_datetime()
+                + timedelta(days=1),
+                contest_time=120,
+                writer="ucpr",
+                description="Welcome",
+                top_content="Welcome",
+                problem_number=4,
+                created_at=create_test_datetime(),
+                updated_at=create_test_datetime(),
+            ).as_json()
+        )
     return result
